527561779591d9053c2f93826811ab8e4a4e8fb4
luisJavierLuna <javierlunaluis@gmail.com>
2022-06-02 15:14:39 -0500

    Added function that forces not to have two candies together in a row or column when the board is just created

--- BoardManager.cs
+++ BoardManager.cs
@@ -7,6 +7,7 @@ public class BoardManager : MonoBehaviour
     [SerializeField] GameObject currentCandy;
     [SerializeField] int xSize, ySize;
     public List<Sprite> sprites = new List<Sprite>();
+    GameObject[,] candies;
 
     private void Start()
     {
@@ -16,8 +17,12 @@ public class BoardManager : MonoBehaviour
 
     void CreateInitialBoard(Vector2 offset)
     {
+        candies = new GameObject[xSize, ySize];
+
         float startX = transform.position.x;
         float startY = transform.position.y;
+
+        int idx = -1;
 
         for (int x = 0; x < xSize; x++)
         {
@@ -30,10 +35,16 @@ public class BoardManager : MonoBehaviour
                         0), 
                     currentCandy.transform.rotation);
                 newCandy.name = string.Format("Candy [{0}][{1}]", x, y);
+                candies[x, y] = newCandy;
                 newCandy.transform.parent = transform;
 
-                Sprite sprite = sprites[Random.Range(0, sprites.Count)];
+                do{ idx = Random.Range(0, sprites.Count); } 
+                while (x > 0 && idx == candies[x - 1, y].GetComponent<Candy>().id ||
+                       y > 0 && idx == candies[x, y - 1].GetComponent<Candy>().id);
+
+                Sprite sprite = sprites[idx];
                 newCandy.GetComponent<SpriteRenderer>().sprite = sprite;
+                newCandy.GetComponent<Candy>().id = idx;
             }
         }
     }
